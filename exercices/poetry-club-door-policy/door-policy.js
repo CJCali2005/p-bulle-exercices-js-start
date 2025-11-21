01426b9056bf3e3da127a5cc4855714467588881
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2025-11-21 14:39:32 +0100

    Feat(poetry-club) : Exercice Terminé.
    [45] [Done]

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line[0];
 }
 
 /**
@@ -38,7 +38,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -49,7 +49,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  const passwordLastLetter = line.trim();
+  return passwordLastLetter[passwordLastLetter.length - 1];
 }
 
 /**
@@ -60,5 +61,9 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return (
+    word.trim()[0].toUpperCase() +
+    word.trim().slice(1).toLowerCase() +
+    ", please"
+  );
 }
