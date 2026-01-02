96cbb31ef799f1243469f6146445f62f5cae05eb
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2026-01-02 21:44:47 +0100

    Feat(space-age): Exercice Termniné
    [55][Done]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,71 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = ( Nomplanete , NBsec) => {
+
+  //Représente le nombre de jours (tours de 24h) éffectué par la planet concernée en 1 an
+  let NBtours = 0;
+  let AgePlanete = 0;
+
+  
+  switch(Nomplanete){
+
+    case "earth":
+
+      NBtours = 365.25 * 1.0;
+
+      break;
+
+    case "mercury":
+      NBtours = 365.25 * 0.2408467;
+
+      break;
+
+    case "venus":
+
+      NBtours = 365.25 * 0.61519726;
+
+      break;
+
+    case "mars":
+
+      NBtours = 365.25 * 1.8808158;
+
+      break;
+
+    case "jupiter":
+
+      NBtours = 365.25 * 11.862615;
+
+      break;
+
+    case "saturn":
+
+      NBtours = 365.25 * 29.447498;
+
+      break;
+
+    case "uranus":
+
+      NBtours = 365.25 * 84.016846;
+
+      break; 
+
+    case "neptune":
+
+      NBtours = 365.25 * 164.79132;
+
+      break;
+
+      default : 
+      throw new Error("not a planet");
+
+  }
+
+  AgePlanete = Math.round(((((NBsec/60)/60) / 24) / NBtours)*100)/100;
+
+  return AgePlanete;
+
 };
+
+
