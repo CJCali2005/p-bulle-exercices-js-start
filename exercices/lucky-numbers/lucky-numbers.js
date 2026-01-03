c8d55ae6a311e9c505ddbcf8b11a258ce397a572
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2026-01-03 17:14:32 +0100

    Feat(lucky-numbers): Exercice terminé (J'ai été bloqué au niveau des messages d'erreur à la fin j'ai siplement inversé les conditions et c'est passé)
    [55][Done]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,8 +8,20 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  let somme1 = 0;
+  let somme2 = 0;
+
+  for (let i = 0; i < array1.length; i++) {
+    somme1 = somme1 * 10 + array1[i];
+  }
+
+  for (let x = 0; x < array2.length; x++) {
+    somme2 = somme2 * 10 + array2[x];
+  }
+ 
+  return somme1 + somme2;
 }
+
 
 /**
  * Checks whether a number is a palindrome.
@@ -18,7 +30,24 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+
+  let a = value.toString().length;
+  let b = value.toString();
+  let c = true ; 
+  let d = 0 ; 
+
+  for (let i = 0 ; i <= a / 2 ; i++)
+  {
+
+    if( b[ i ] !== b[ a - 1 - i ] )
+    {
+      return false;
+    }
+
+  }
+
+  return true;
+  
 }
 
 /**
@@ -29,5 +58,20 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+
+  if(input === null || input === undefined || input === '')
+  {
+    return "Required field";
+  }
+  else if ( isNaN(Number(input)) && input !== '' || Number(input) < 1 && input !== ''){
+
+    return  "Must be a number besides 0";
+
+  }
+  else{
+    return '' ;
+  }
+
+
+  
 }
