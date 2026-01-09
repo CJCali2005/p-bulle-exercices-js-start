8506ea8afdbe6d7bb5010c08086585ed5cd97b0a
CJCali2005 <235065072+CJCali2005@users.noreply.github.com>
2026-01-09 10:41:42 +0100

    Feat(pascals-triangle): Exercice Terminé (Blocage à cause des spec xtest --> test parce que ça skippait)
    [65][Done]

--- exercices/pascals-triangle/pascals-triangle.js
+++ exercices/pascals-triangle/pascals-triangle.js
@@ -3,6 +3,35 @@
 // convenience to get you started writing code faster.
 //
 
-export const rows = () => {
-  throw new Error('Remove this line and implement the function');
+export const rows = (n) => {
+  if (n <= 0) return [];
+
+  let triangle = [];
+  
+  // première ligne 
+  let b = [1];
+  triangle.push([...b]); // on copie la ligne dans le triangle
+
+  for (let ligne = 1; ligne < n; ligne++) {
+    let c = new Array(b.length + 1); // nouvelle ligne
+
+    // 1 de la première case
+    c[0] = 1;
+
+    // somme 2 par 2 premier tour b.length = 0
+    for (let i = 0; i < b.length - 1; i++) {
+      c[i + 1] = b[i] + b[i + 1];
+    }
+
+    // 1 à la fin
+    c[c.length - 1] = 1;
+
+    // ajouter la ligne au triangle
+    triangle.push([...c]);
+
+    // préparer b pour la prochaine boucle
+    b = c;
+  }
+
+  return triangle;
 };
